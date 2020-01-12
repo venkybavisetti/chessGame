6a07f2bd9557c4277c4726768f2e1219aa7bf4a6
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-01-12 12:01:32 +0530

    pawn moving

--- jsCode.js
+++ jsCode.js
@@ -4,7 +4,8 @@ const NUM_OF_ROWS = 8;
 const getTable = () => document.getElementById("tableGrid");
 
 const createRow = function(tableRow, colorDecider) {
-  const cell = document.createElement("th");
+  const cell = document.createElement("td");
+  cell.addEventListener("click", runPieces.bind(null, cell));
   const color = colorDecider == 0 ? "white" : "black";
   cell.className = color;
   tableRow.appendChild(cell);
@@ -34,6 +35,17 @@ class Bishop {
   }
   get image() {
     return this.img;
+  }
+  get possibleMoves() {
+    let [row, colum] = this.position;
+    let possibilities = [];
+    for (let increment = 0; increment < 8; increment++) {
+      if (increment != row) possibilities.push([increment, colum]);
+    }
+    for (let increment = 0; increment < 8; increment++) {
+      if (increment != colum) possibilities.push([row, increment]);
+    }
+    return possibilities;
   }
 }
 
@@ -69,7 +81,7 @@ class BlackPawn {
     this.img = img;
   }
   get possibleMoves() {
-    const [row, colum] = this.position;
+    let [row, colum] = this.position;
     return [++row, colum];
   }
   get location() {
@@ -78,7 +90,12 @@ class BlackPawn {
   get image() {
     return this.img;
   }
+  move1Step() {
+    let [row, colum] = this.position;
+    this.position = [++row, colum];
+  }
 }
+
 class WhitePawn {
   constructor(position, img) {
     this.position = position.slice();
@@ -91,7 +108,11 @@ class WhitePawn {
     return this.img;
   }
   get possibleMoves() {
-    [row, colum] = this.position;
+    let [row, colum] = this.position;
+    return [--row, colum];
+  }
+  move1Step() {
+    let [row, colum] = this.position;
     return [--row, colum];
   }
 }
@@ -120,59 +141,87 @@ class Rook {
   get image() {
     return this.img;
   }
+  get possibleMoves() {
+    let [row, colum] = this.position;
+    let possibilities = [];
+    for (let increment = 0; increment < 8; increment++) {
+      if (increment != row) possibilities.push([increment, colum]);
+    }
+    for (let increment = 0; increment < 8; increment++) {
+      if (increment != colum) possibilities.push([row, increment]);
+    }
+    return possibilities;
+  }
 }
 
 const displayPiecesPositions = function(chessPiecesPositions) {
   let table = getTable();
   for (const piece in chessPiecesPositions) {
-    console.log(piece);
     const [x, y] = chessPiecesPositions[piece].location;
     table.rows[x].cells[
       y
-    ].innerHTML = `<img src=${chessPiecesPositions[piece].image} alt =${piece} height ='80' width ='70'/>`;
+    ].innerHTML = `<img src=${chessPiecesPositions[piece].image} alt =${piece} id=${piece} height ='80' width ='60' />`;
   }
 };
-const BlackChessPieces = {
-  blackRookLeft: new Rook([0, 0], "./chessImages/blackRook.png"),
-  blackRookRight: new Rook([0, 7], "./chessImages/blackRook.png"),
-  blackKnightLeft: new Knight([0, 1], "./chessImages/blackKnight.png"),
-  blackKnightRight: new Knight([0, 6], "./chessImages/blackKnight.png"),
-  blackBishopLeft: new Bishop([0, 2], "./chessImages/blackBishop.png"),
-  blackBishopRight: new Bishop([0, 5], "./chessImages/blackBishop.png"),
-  blackQueen: new Queen([0, 3], "./chessImages/blackQueen.png"),
-  blackKing: new King([0, 4], "./chessImages/blackKing.png"),
+const getBlackChessPieces = function() {
+  return {
+    blackRookLeft: new Rook([0, 0], "./chessImages/blackRook.png"),
+    blackRookRight: new Rook([0, 7], "./chessImages/blackRook.png"),
+    blackKnightLeft: new Knight([0, 1], "./chessImages/blackKnight.png"),
+    blackKnightRight: new Knight([0, 6], "./chessImages/blackKnight.png"),
+    blackBishopLeft: new Bishop([0, 2], "./chessImages/blackBishop.png"),
+    blackBishopRight: new Bishop([0, 5], "./chessImages/blackBishop.png"),
+    blackQueen: new Queen([0, 3], "./chessImages/blackQueen.png"),
+    blackKing: new King([0, 4], "./chessImages/blackKing.png"),
 
-  blackPawn1: new BlackPawn([1, 0], "./chessImages/blackPawn.png"),
-  blackPawn2: new BlackPawn([1, 1], "./chessImages/blackPawn.png"),
-  blackPawn3: new BlackPawn([1, 2], "./chessImages/blackPawn.png"),
-  blackPawn4: new BlackPawn([1, 3], "./chessImages/blackPawn.png"),
-  blackPawn5: new BlackPawn([1, 4], "./chessImages/blackPawn.png"),
-  blackPawn6: new BlackPawn([1, 5], "./chessImages/blackPawn.png"),
-  blackPawn7: new BlackPawn([1, 6], "./chessImages/blackPawn.png"),
-  blackPawn8: new BlackPawn([1, 7], "./chessImages/blackPawn.png")
+    blackPawn1: new BlackPawn([1, 0], "./chessImages/blackPawn.png"),
+    blackPawn2: new BlackPawn([1, 1], "./chessImages/blackPawn.png"),
+    blackPawn3: new BlackPawn([1, 2], "./chessImages/blackPawn.png"),
+    blackPawn4: new BlackPawn([1, 3], "./chessImages/blackPawn.png"),
+    blackPawn5: new BlackPawn([1, 4], "./chessImages/blackPawn.png"),
+    blackPawn6: new BlackPawn([1, 5], "./chessImages/blackPawn.png"),
+    blackPawn7: new BlackPawn([1, 6], "./chessImages/blackPawn.png"),
+    blackPawn8: new BlackPawn([1, 7], "./chessImages/blackPawn.png")
+  };
 };
 
-const WhiteChessPieces = {
-  whiteRookLeft: new Rook([7, 0], "./chessImages/whiteRook.png"),
-  whiteRookRight: new Rook([7, 7], "./chessImages/whiteRook.png"),
-  whiteKnightLeft: new Knight([7, 1], "./chessImages/whiteKnight.png"),
-  whiteKnightRight: new Knight([7, 6], "./chessImages/whiteKnight.png"),
-  whiteBishopLeft: new Bishop([7, 2], "./chessImages/whiteBishop.png"),
-  whiteBishopRight: new Bishop([7, 5], "./chessImages/whiteBishop.png"),
-  whiteQueen: new Queen([7, 3], "./chessImages/whiteQueen.png"),
-  whiteKing: new King([7, 4], "./chessImages/whiteKing.png"),
+const getWhiteChessPieces = function() {
+  return {
+    whiteRookLeft: new Rook([7, 0], "./chessImages/whiteRook.png"),
+    whiteRookRight: new Rook([7, 7], "./chessImages/whiteRook.png"),
+    whiteKnightLeft: new Knight([7, 1], "./chessImages/whiteKnight.png"),
+    whiteKnightRight: new Knight([7, 6], "./chessImages/whiteKnight.png"),
+    whiteBishopLeft: new Bishop([7, 2], "./chessImages/whiteBishop.png"),
+    whiteBishopRight: new Bishop([7, 5], "./chessImages/whiteBishop.png"),
+    whiteQueen: new Queen([7, 3], "./chessImages/whiteQueen.png"),
+    whiteKing: new King([7, 4], "./chessImages/whiteKing.png"),
 
-  whitePawn1: new WhitePawn([6, 0], "./chessImages/whitePawn.png"),
-  whitePawn2: new WhitePawn([6, 1], "./chessImages/whitePawn.png"),
-  whitePawn3: new WhitePawn([6, 2], "./chessImages/whitePawn.png"),
-  whitePawn4: new WhitePawn([6, 3], "./chessImages/whitePawn.png"),
-  whitePawn5: new WhitePawn([6, 4], "./chessImages/whitePawn.png"),
-  whitePawn6: new WhitePawn([6, 5], "./chessImages/whitePawn.png"),
-  whitePawn7: new WhitePawn([6, 6], "./chessImages/whitePawn.png"),
-  whitePawn8: new WhitePawn([6, 7], "./chessImages/whitePawn.png")
+    whitePawn1: new WhitePawn([6, 0], "./chessImages/whitePawn.png"),
+    whitePawn2: new WhitePawn([6, 1], "./chessImages/whitePawn.png"),
+    whitePawn3: new WhitePawn([6, 2], "./chessImages/whitePawn.png"),
+    whitePawn4: new WhitePawn([6, 3], "./chessImages/whitePawn.png"),
+    whitePawn5: new WhitePawn([6, 4], "./chessImages/whitePawn.png"),
+    whitePawn6: new WhitePawn([6, 5], "./chessImages/whitePawn.png"),
+    whitePawn7: new WhitePawn([6, 6], "./chessImages/whitePawn.png"),
+    whitePawn8: new WhitePawn([6, 7], "./chessImages/whitePawn.png")
+  };
+};
+
+const erasePreviousStep = function(parent, child) {
+  parent.removeChild(child);
+};
+
+const runPieces = function(parent) {
+  const child = parent.childNodes[0];
+  const requirePiece = BlackChessPieces[child.id];
+  requirePiece.move1Step();
+  displayPiecesPositions(BlackChessPieces);
+  erasePreviousStep(parent, child);
 };
 
 const main = function() {
+  WhiteChessPieces = { ...getWhiteChessPieces() };
+  BlackChessPieces = { ...getBlackChessPieces() };
   createChessBoard();
   displayPiecesPositions(WhiteChessPieces);
   displayPiecesPositions(BlackChessPieces);
